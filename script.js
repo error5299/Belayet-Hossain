d427ce1029193bc01f875085b63e234f95007e0b
Belayet Hossein <122087994+error5299@users.noreply.github.com>
2025-04-13 08:55:32 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-document.getElementById('contactForm').addEventListener('submit', function (e) {
-  e.preventDefault();
-  alert('Thank you for your message!');
-  this.reset(); // Clear form
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Page loaded!");
 });
